0d460c3ef6ed65e969378eb2faed6683315ca3c8
code2nvim <code2nvim@gmail.com>
2024-09-19 23:36:46 +0800

    remove Menu

--- routes/(_components)/Header.tsx
+++ routes/(_components)/Header.tsx
@@ -1,20 +1,13 @@
-import { useSignal } from "@preact/signals";
-import Menu from "../(_islands)/Menu.tsx";
-import MenuButton from "../(_islands)/MenuButton.tsx";
 import { Button } from "./Button.tsx";
 
 export function Header() {
-  const showMenu = useSignal(false);
-  return (
-    <>
-      <header class="py-1 flex justify-between items-center bg-green-700">
-        <a href="/" class="m-0">
-          <Button>HOME</Button>
-        </a>
-        <h1>tools-website</h1>
-        <MenuButton showMenu={showMenu} />
-      </header>
-      <Menu show={showMenu} />
-    </>
-  );
+	return (
+		<>
+			<header class="py-1 flex justify-between items-center bg-green-700">
+				<a href="/" class="m-0">
+					<Button>HOME</Button>
+				</a>
+			</header>
+		</>
+	);
 }
