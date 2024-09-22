da316294fe872c36c7a72c2c8a7b6aa3b4b1231e
code2nvim <code2nvim@gmail.com>
2024-09-22 22:45:56 +0800

    badge: "Made with Fresh"

--- routes/(_components)/Header.tsx
+++ routes/(_components)/Header.tsx
@@ -1,13 +1,21 @@
 import { Button } from "./Button.tsx";
 
 export function Header() {
-	return (
-		<>
-			<header class="py-1 flex justify-between items-center bg-green-700">
-				<a href="/" class="m-0">
-					<Button>HOME</Button>
-				</a>
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header class="py-1 flex justify-between items-center bg-green-700">
+        <a href="/" class="m-0">
+          <Button>HOME</Button>
+        </a>
+        <a href="https://fresh.deno.dev">
+          <img
+            width="197"
+            height="37"
+            src="https://fresh.deno.dev/fresh-badge.svg"
+            alt="Made with Fresh"
+          />
+        </a>
+      </header>
+    </>
+  );
 }
