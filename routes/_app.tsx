ce549b335140a7222dc503ec2d040b27b39a84b4
code2nvim <code2nvim@gmail.com>
2025-01-24 19:53:46 +0800

    add website icon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,8 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>tools-website</title>
+        <title>Tools Website</title>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
